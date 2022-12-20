ed4626cbc4919ad50735b531aba6c11fe18430a8
Danubio de Araújo <danubio.bwm@gmail.com>
2022-12-20 00:11:49 -0300

    JSON

---
+++ Json/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Conta struct {
+	Numero int `json: "n"`
+	Saldo  int `json: "s"`
+}
+
+func main() {
+	conta := Conta{Numero: 1, Saldo: 100}
+
+	res, err := json.Marshal(conta)
+
+	if err != nil {
+		println(err)
+	}
+	println(string(res))
+
+	err = json.NewEncoder(os.Stdout).Encode(conta)
+
+	if err != nil {
+		println(err)
+	}
+	println(string(res))
+
+	jsonPuro := []byte(`{n: 1, s: 100}`)
+
+	var contaX Conta
+	err = json.Unmarshal(jsonPuro, &contaX)
+
+	println(contaX.Saldo)
+}
